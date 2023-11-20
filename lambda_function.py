ff1d52f49516ab4bea96c41c39e65eac4556add2
NickFamiglietti <117298513+NickFamiglietti@users.noreply.github.com>
2023-11-20 16:55:04 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,28 +1,9 @@
 import json
-import requests
 
-def lambda_handler(event, context):
-    
-    post_body = event['body']
-    
-    try:
-        data = json.loads(post_body)
-       # target_url = 'link'  
-        response = requests.post(target_url, json=data)
-        
-        if response.status_code == 200:
-            return {
-                'statusCode': 200,
-                'body': json.dumps({'message': 'Data sent successfully'})
-            }
-        else:
-            return {
-                'statusCode': response.status_code,
-                'body': json.dumps({'message': 'Failed to send data'})
-            }
-    
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
-        }
+def lamba_handler(event, context):
+    j = json.oads(event)
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'text': f'Issue Created: {j["issue"]["html_url"]}'})
+
+    }
