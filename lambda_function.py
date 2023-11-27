2376f7f5c8308d98d6e0965a048917240238f165
NickFamiglietti <117298513+NickFamiglietti@users.noreply.github.com>
2023-11-27 11:55:06 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,15 @@
+import os
 import json
+import urllib3
 
-def lamba_handler(event, context):
-    j = json.oads(event)
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'text': f'Issue Created: {j["issue"]["html_url"]}'})
 
-    }
+def lambda_handler(event, context):
+    # print(event)
+    # print(type(event))
+    # print(dir(event))
+    # j = json.loads(event)
+    payload = json.dumps({'text': f'Issue Created: {event["issue"]["html_url"]}'})
+    url = os.getenv("SLACK_URL")
+    http = urllib3.PoolManager()
+    response = http.request("POST", url, body=payload, headers={"Content-Type": "application/json"})
+    return response.data.decode()
